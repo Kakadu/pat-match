760505ddd7f91f476cacff415afa9d62265a7fd9
kakadu <kakadu.hafanana@gmail.com>
2020-05-17 20:52:55 +0300

    Some work about other way to generate examples (probably dead born one but it started some refactoring)

--- switch/algo_fair_manual.ml
+++ switch/algo_fair_manual.ml
@@ -1,95 +1,56 @@
 (* Executes exactly what is written in lozovML
  *
- * Downside: doesn't know that match in Pair is not okay
  *)
 open Main_inputs
 open Unn_pre
 open OCanren
 open Unn_pre.IR
 
-let is_enabled = ref true
-
-(*
-module Work2 = struct
-  include Work
-
-let rec not_in_history x xs = Work.not_in_history x xs !!true
-
-let rec eval_ir (s: Expr.injected) max_height tinfo shortcut1 shortcut_tag ir q32 =
-  let open OCanren.Std in
-  let rec inner history test_list  irrr q11 =
-    conde
-      [ (irrr === (fail ())) &&& (q11 === (none ()))
-      ; fresh (n)
-          (irrr === (lit n))
-          (q11 === (some n))
-      ; fresh (m cases on_default q15 q17 etag args cnames q19)
-          (irrr === (switch m cases on_default))
-          (cases =/= nil())
-          (q17 === (pair (eConstr etag args) cnames))
-          (matchable_leq_nat m max_height !!true)                  (* commenting this line is not recommended *)
-          (eval_m s tinfo m q17)
-          (shortcut1 etag m cases history !!true)
-          (* (not_in_history m history) *)
-          (test_list (m%history) etag  cnames on_default cases q11)
-      ]
-  in
-  let rec test_list next_histo (etag: Tag.injected) cnames on_default cases0 r =
-    let rec helper (constr_names: (Tag.ground, Tag.logic) Std.List.groundi) cases rez =
-      conde
-        [ fresh ()
-            (cases === (nil ()))
-            (inner next_histo test_list on_default rez)
-        ; fresh (constr_hd constr_tl)
-            (cases =/= (nil ()))
-            (conde
-              [ (constr_names === nil()) &&& failure
-              ; fresh (u)
-                  (constr_names === u % (nil()))
-                  (cases === nil())
-              ; fresh (u v w)
-                  (constr_names === u % (v % w) )
-(*
-              ; fresh (u v)
-                  (constr_names === u % v )
-*)
-              ])
-
-            (fresh (qtag ontag clauses_tl)
-              (constr_names === (constr_hd % constr_tl))
-              (cases === ((pair qtag ontag) % clauses_tl))
-              (conde
-                [ (qtag === constr_hd) &&&
-                  (conde
-                    [ (qtag === etag) &&& (inner next_histo test_list ontag rez)
-                    ; (qtag =/= etag) &&& (helper constr_tl clauses_tl rez)
-                    ])
-                ; (qtag =/= constr_hd) &&& (helper constr_tl cases rez)
-                ])
-            )
-         ]
-    in
-    helper cnames cases0 r
-  in
-  inner (nil()) test_list ir q32
-end
-
-let (_: Expr.injected -> N.injected -> Typs.injected -> _ -> _ -> IR.injected -> _ -> goal) = Work.eval_ir
-
 let disable_periodic_prunes () =
   let open OCanren.PrunesControl in
   enable_skips ~on:false
+
+
+(* ******************** Default synthesis shortucts ************************* *)
+let default_shortcut0 m max_height cases rez =
+  let open OCanren in
+  (Work.matchable_leq_nat m max_height !!true) &&&
+  (cases =/= Std.nil()) &&&
+  (rez === !!true)
+
+let default_shortcut etag m cases history rez =
+  let open OCanren in
+  (Work.not_in_history m history !!true)
+
+let default_shortcut_tag constr_names cases rez =
+  let open OCanren in
+  let open OCanren.Std in
+  (conde
+    [ (constr_names === nil()) &&& failure
+    ; fresh (u)
+        (constr_names === u % (nil()))
+        (cases === nil())
+    ; fresh (u v w)
+        (constr_names === u % (v % w) )
+    ])
+
 
 exception FilteredOutBySize of int
 exception FilteredOutByForm
 exception FilteredOutByNestedness
 
+let is_enabled = ref true
 
 module Make(Arg: ARG_FINAL) = struct
-
-  let work ?(n=10) ~with_hack ~print_examples ~check_repeated_ifs ~debug_filtered_by_size ~prunes_period clauses typs =
+  let work ?(n=10) ~with_hack ~print_examples ~check_repeated_ifs ~debug_filtered_by_size
+          ~prunes_period ~with_default_shortcuts clauses typs =
     print_endline "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
-    Clauses.pretty_print Format.std_formatter clauses;
+
+    let printed_clauses = Format.asprintf "%a" Clauses.pretty_print clauses in
+    Format.printf "%s" printed_clauses;
+    Mybench.set_start_info Arg.info ~n prunes_period ~clauses:printed_clauses;
+
+
     let possible_answer = Arg.possible_answer in
     assert (Arg.max_ifs_count <= (IR.count_ifs_ground possible_answer));
     let max_ifs_count = ref Arg.max_ifs_count in
@@ -187,26 +148,92 @@ module Make(Arg: ARG_FINAL) = struct
             then Format.printf "  %s \x1b[31mFILTERED OUT\x1b[39;49m because by nestedness\n%!"
                 (IR.show_logic ir) ;
             false
+
       )
     in
 
 (*    let (_: Expr.injected -> Nat.injected -> Typs.injected -> _ -> IR.injected -> _ -> goal) = Work.eval_ir in*)
 (*    let (_: IR.injected -> Expr.injected -> Typs.injected -> IR.injected -> _ -> goal) = my_eval_ir in*)
 
+    let rec ground_append xs ys =
+      match xs with
+      | Std.List.Cons (x, xs) -> Std.List.Cons (x, ground_append xs ys)
+      | Std.List.Nil -> ys
+    in
+    let rec ground_concat xs =
+      match xs with
+      | Std.List.Nil -> Std.List.Nil
+      | Std.List.Cons (xs, tl) -> ground_append xs (ground_concat tl)
+    in
+
+
+    let rec cartesian (xss : 'a Std.List.ground Std.List.ground) =
+      match xss with
+      | Std.List.Nil -> Std.List.Nil
+      | Std.List.Cons (x, xs) ->
+          let rest = cartesian xs in
+          ground_concat @@
+          GT.gmap Std.List.ground
+            (fun k -> GT.gmap Std.List.ground (fun xs -> Std.List.Cons(k,xs)) rest) x
+    in
+
     let injected_clauses = Clauses.inject clauses in
     let injected_exprs =
       let demo_exprs =
-        run one (fun q -> Arg.inhabit Arg.max_height q) (fun r -> r#prjc Arg.prjp)
+        run one (fun q -> Arg.inhabit Arg.max_height q) (fun r -> r#prjc Expr.prjc )
         |> OCanren.Stream.take ~n:(-1)
-        |> Arg.to_expr
       in
 
+      let extra_exprs =
+        let rec helper (h: int) (pat: Pattern.ground) : Expr.ground Std.List.ground =
+          match pat with
+          | Pattern.WildCard -> Std.List.Nil
+          | PConstr (name, ps) ->
+              let notbad (q: Expr.injected) =
+                match ps with
+                | Std.List.Nil -> (q =/= (Expr.constr (Tag.inject name) (Std.nil())))
+                | Std.List.Cons (_, Std.List.Nil) -> Fresh.one (fun x -> q =/= Std.(Expr.constr Tag.(inject name) !<x))
+                | _ -> assert false
+              in
+              let new_examples =
+                run q (fun (q: Expr.injected) -> (notbad q) &&& (Arg.inhabit h q))
+                  (fun r -> r#prjc Expr.prjc)
+                |> OCanren.Stream.take ~n:(-1)
+                |> Std.List.of_list id
+              in
+              let ps_awsers =
+                GT.gmap Std.List.ground (helper (h-1)) ps
+                |> cartesian
+                |> GT.gmap Std.List.ground (fun xs -> Expr.EConstr (name, xs))
+              in
+              ground_append ps_awsers new_examples
+        in
+        List.concat @@
+        List.map (fun (p,_) -> helper 3 p |> Std.List.to_list id) clauses
+      in
+
+      let all_exprs = demo_exprs in
+(*      let all_exprs = demo_exprs @ extra_exprs in*)
+      let all_exprs =
+          Base.List.dedup_and_sort
+            ~compare:(fun a b ->
+              match GT.compare Expr.ground a b with
+                | GT.LT -> -1
+                | GT.EQ -> 0
+                | GT.GT -> 1)
+            all_exprs
+      in
+      let all_exprs = Base.List.sort ~compare:(fun a b -> compare (Expr.height a) (Expr.height b)) all_exprs
+      in
       let () =
         if print_examples
-        then Format.printf "Testing %d examples:\n%!" (List.length demo_exprs);
+        then Format.printf "Testing %d examples:\n%!" (List.length all_exprs);
 
-        demo_exprs |> List.iter (fun e ->
-          if print_examples then Format.printf "  %s ~~> %!" (Expr.show e);
+
+        all_exprs |> List.iter (fun e ->
+          if print_examples then Format.printf "  %s -> %!" (Expr.show e);
+
+
           let answer_demo = IR.inject possible_answer in
           let scru_demo = Expr.inject e in
           let open OCanren in
@@ -215,7 +242,7 @@ module Make(Arg: ARG_FINAL) = struct
                 (Work.eval_pat scru_demo injected_clauses rez)
                 (rez === Std.Option.some ir)
                 (ir === IR.int n)
-                (Work.eval_ir scru_demo max_height typs simple_shortcut simple_shortcut_tag answer_demo (Std.Option.some n))
+                (Work.eval_ir scru_demo max_height typs simple_shortcut0 simple_shortcut simple_shortcut_tag answer_demo (Std.Option.some n))
             )
             (fun r -> r)
             |> (fun s ->
@@ -226,8 +253,9 @@ module Make(Arg: ARG_FINAL) = struct
                         let open Mytester in
                         runR (Std.Option.reify OCanren.reify)
                           (fun ~span:_ -> GT.show Std.Option.ground @@ GT.show GT.int)
-                          (fun ~span:_ -> GT.show Std.Option.logic (GT.show logic @@ GT.show GT.int)) 1 q qh
-                          ("eval_ir", (Work.eval_ir scru_demo max_height typs simple_shortcut simple_shortcut_tag answer_demo))
+                          (fun ~span:_ -> GT.show Std.Option.logic (GT.show logic @@ GT.show GT.int))
+                          1 q qh
+                          ("eval_ir", (Work.eval_ir scru_demo max_height typs simple_shortcut0 simple_shortcut simple_shortcut_tag answer_demo))
                       in
                       failwith "Bad (?) example"
                   in
@@ -235,9 +263,11 @@ module Make(Arg: ARG_FINAL) = struct
                   then Format.printf "%s\n%!" @@
                         IR.show_logic ((OCanren.Stream.hd s)#reify IR.reify);
             )
+
           )
       in
-      List.map Expr.inject demo_exprs
+
+      List.map Expr.inject all_exprs
     in
 
     let () =
@@ -250,21 +280,56 @@ module Make(Arg: ARG_FINAL) = struct
           reset ()
       | None -> disable_periodic_prunes ()
     in
-    let info = Format.sprintf "MANUAL lozovML (%s)" Arg.info in
+    let info = Format.sprintf "fair lozovML (%s)" Arg.info in
+
+
+
+    let answer_index = ref (-1) in
     let on_ground ~span ir =
+      incr answer_index;
       let nextn = IR.count_ifs_ground ir in
       upgrade_bound nextn;
-      IR.show ir
+      let repr = Printf.sprintf "%s with ifs_low=%d" (IR.show ir) nextn in
+      Mybench.when_enabled ~fail:(fun () -> repr)
+        (fun () ->
+          Mybench.got_answer span ~idx:(!answer_index);
+          repr)
     in
     let on_logic ~span ir =
+      incr answer_index;
       let nextn = IR.count_ifs_low ir in
       upgrade_bound nextn;
-      IR.show_logic ir
+      let repr = Printf.sprintf "%s with ifs_low=%d" (IR.show_logic ir) nextn in
+      Mybench.when_enabled ~fail:(fun () -> repr)
+        (fun () ->
+          Mybench.got_answer span ~idx:(!answer_index);
+          repr)
     in
 
+    let shortcut0 m maxheight cases rez =
+      (Arg.shortcut0 m maxheight cases rez) &&&
+      (if with_default_shortcuts
+       then default_shortcut0 m maxheight cases rez
+       else success)
+    in
+    let shortcut1 etag m cases history rez =
+      (Arg.shortcut etag m cases history rez) &&&
+      (if with_default_shortcuts
+       then default_shortcut etag m cases history rez
+       else success)
+    in
+    let shortcut_tag1 constr_names cases rez =
+      (Arg.shortcut_tag constr_names cases rez) &&&
+      (if with_default_shortcuts
+       then default_shortcut_tag constr_names cases rez
+       else success)
+    in
     let my_eval_ir ideal (s: Expr.injected) tinfo ir rez =
       (if with_hack then _ifs_size_hack ideal else success) &&&
-      (Work2.eval_ir s max_height tinfo Arg.shortcut Arg.shortcut_tag ir rez)
+      (* There we use shortcuts optimized for search.
+       * These shortcuts canptentially broke execution in default direction
+       *)
+      (Work.eval_ir s max_height tinfo shortcut0 shortcut1 shortcut_tag1 ir rez)
     in
 
     let start = Mtime_clock.counter () in
@@ -304,13 +369,13 @@ module Make(Arg: ARG_FINAL) = struct
 
   let test ?(print_examples=true) ?(debug_filtered_by_size=false)
       ?(with_hack=true) ?(check_repeated_ifs=false)
-      ?(prunes_period=(Some 100))
+      ?(prunes_period=(Some 100)) ?(with_default_shortcuts=true)
       n =
-      if !is_enabled
-      then work ~n ~with_hack ~print_examples ~check_repeated_ifs ~debug_filtered_by_size
-        ~prunes_period
-        Arg.clauses Arg.typs
-      else ()
+    if !is_enabled
+    then work ~n ~with_hack ~print_examples ~check_repeated_ifs
+      ~debug_filtered_by_size ~with_default_shortcuts
+      ~prunes_period
+      Arg.clauses Arg.typs
+    else ()
 end
 
-*)
