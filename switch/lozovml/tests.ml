c06be5ff07ca315f4f4604fad94240bf2f44a7a6
Kakadu <Kakadu@pm.me>
2024-08-22 23:54:09 +0300

    A few tests about Lozovml code
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ switch/lozovml/tests.ml
@@ -0,0 +1,79 @@
+(* let%expect_test _ =
+   let open Unn_pre in
+   let clauses = [ (ptrue, IR.eint 1); (pfalse, IR.eint 0) ] in
+
+   let injected : Clauses.injected = Clauses.inject clauses in
+   let ans =
+     OCanren.(run q (Work_base_common.compile_naively injected)) (fun rr ->
+         rr#reify IR.prj_exn)
+     |> OCanren.Stream.hd
+   in
+   print_endline (GT.show IR.ground ans);
+   [%expect {||}] *)
+
+let trace_pats ps =
+  let open OCanren in
+  debug_var ps (Std.List.prj_exn Unn_pre.Pattern.prj_exn) (function
+    | [ ps ] ->
+        print_endline "HERR";
+        Format.printf "@[[%s]@]@ %!"
+          (String.concat " " @@ List.map Unn_pre.Pattern.show ps);
+        success
+    | _ -> assert false)
+
+let%expect_test _ =
+  let open Unn_pre in
+  let clauses =
+    [
+      (ppair ptrue __, IR.eint 1);
+      (ppair __ ptrue, IR.eint 1);
+      (ppair pfalse pfalse, IR.eint 0);
+    ]
+  in
+  let injected : Clauses.injected = Clauses.inject clauses in
+  let ans =
+    OCanren.(run q) (Work_base_common.compile_naively injected) (fun rr ->
+        rr#reify IR.prj_exn)
+    |> OCanren.Stream.hd
+  in
+  print_endline (GT.show IR.ground ans);
+  [%expect {| (match S with  | pair -> (match S[0] with  | true -> 1   | _ -> (match S with  | pair -> (match S[1] with  | true -> 1   | _ -> (match S with  | pair -> (match S[1] with  | false -> (match S[0] with  | false -> 0   | _ -> fail)  | _ -> fail)  | _ -> fail))  | _ -> (match S with  | pair -> (match S[1] with  | false -> (match S[0] with  | false -> 0   | _ -> fail)  | _ -> fail)  | _ -> fail)))  | _ -> (match S with  | pair -> (match S[1] with  | true -> 1   | _ -> (match S with  | pair -> (match S[1] with  | false -> (match S[0] with  | false -> 0   | _ -> fail)  | _ -> fail)  | _ -> fail))  | _ -> (match S with  | pair -> (match S[1] with  | false -> (match S[0] with  | false -> 0   | _ -> fail)  | _ -> fail)  | _ -> fail))) |}]
+
+let traco file line =
+  let open OCanren in
+  debug_var !!1 OCanren.reify (fun _ ->
+      Printf.printf "%s %d\n" file line;
+      success)
+(*
+let%expect_test _ =
+  let open OCanren in
+  let list_mapi f =
+    let rec helper i xs q18 =
+      conde
+        [
+          fresh () (traco __FILE__ __LINE__)
+            (xs === Std.List.nil ())
+            (traco __FILE__ __LINE__)
+            (q18 === Std.nil ())
+            (traco __FILE__ __LINE__);
+          fresh (x q20 q21 q22) (traco __FILE__ __LINE__)
+            (xs === Std.List.cons x q20)
+            (traco __FILE__ __LINE__)
+            (q18 === Std.List.cons q21 q22)
+            (f i x q21)
+            (helper (Std.Nat.succ i) q20 q22);
+        ]
+    in
+    fun xs q24 -> traco __FILE__ __LINE__ &&& helper Std.Nat.zero xs q24
+  in
+  let injected : int ilogic Std.List.injected = OCanren.Std.list ( !! ) [ 1 ] in
+  let _ =
+    OCanren.(run q)
+      (list_mapi
+         (fun i x q17 -> traco __FILE__ __LINE__ &&& (i === q17))
+         injected)
+      (fun rr -> rr#reify (Std.List.reify OCanren.reify))
+    |> OCanren.Stream.hd
+  in
+  ();
+  [%expect {||}] *)
--- switch/lozovml/work_base.ml
+++ switch/lozovml/work_base.ml
@@ -1,14 +1,5 @@
-(* open Peano
-open List
- *)
-
-(* open OCanren.Std *)
-
 type nat = Z | S of nat
-
-type pattern =
-  | WildCard
-  | PConstr of int * pattern list
+type pattern = WildCard | PConstr of int * pattern list
 type expr = EConstr of int * expr list
 
 type typ_info =
@@ -16,77 +7,68 @@ type typ_info =
   | T of (int * typ_info list) list
 
 type matchable = Scru | Field of nat * matchable
-type ir =
-  | Fail
-  | Switch of matchable * (int * ir) list * ir
-  | Lit of int
+type ir = Fail | Switch of matchable * (int * ir) list * ir | Lit of int
 
 let well_typed_expr_height height default e typs =
   let rec list_assoc name ys =
-    match ys with
-    | (k,v)::xs ->
-        if k = name
-        then v
-        else list_assoc name xs
+    match ys with (k, v) :: xs -> if k = name then v else list_assoc name xs
   in
   let info_assoc tt name = match tt with T xs -> list_assoc name xs in
-  let rec list_all2 f xs0 ys0 = match xs0,ys0 with
-    | (x::xs, y::ys) ->
-        if f x y
-        then list_all2 f xs ys
-        else false
-    | [],[] -> true
-    | (_::_,[]) -> false
-    | ([], _::_) -> false
+  let rec list_all2 f xs0 ys0 =
+    match (xs0, ys0) with
+    | x :: xs, y :: ys -> if f x y then list_all2 f xs ys else false
+    | [], [] -> true
+    | _ :: _, [] -> false
+    | [], _ :: _ -> false
   in
   let rec helper height e typs =
     match height with
-    | Z -> (e=default)
-    | S n ->
-        match e,typs with
-        | (EConstr (tag, es), ts) ->
+    | Z -> e = default
+    | S n -> (
+        match (e, typs) with
+        | EConstr (tag, es), ts ->
             let arg_infos = info_assoc typs tag in
-            list_all2 (helper n) es arg_infos
+            list_all2 (helper n) es arg_infos)
   in
   helper height e typs
 
-let compile_naively pats : ir =
-  let rec list_combine xs ys =
-    match xs,ys with
-    | [],[] -> []
-    | x::xs,y::ys -> (x, y) :: (list_combine xs ys)
-  in
-  let list_mapi f xs =
-    let rec helper i xs = match xs with
-    | [] -> []
-    | x::xs -> (f i x) :: helper (S i) xs
-    in
-    helper Z xs
-  in
-  let list_decorate_nat xs =
-    list_combine (list_mapi (fun i x -> i) xs) xs
-  in
-  let rec list_foldl f acc ys = match ys with
-  | [] -> acc
-  | x::xs -> list_foldl f (f acc x) xs
-  in
+let rec list_combine xs ys =
+  match (xs, ys) with
+  | [], [] -> []
+  | x :: xs, y :: ys -> (x, y) :: list_combine xs ys
 
+let list_mapi f xs =
+  let rec helper i ys =
+    (* Beware of the bugs
+       https://github.com/PLTools/noCanren/pull/27 *)
+    match ys with [] -> [] | x :: xs -> f i x :: helper (S i) xs
+  in
+  helper Z xs
+
+let list_decorate_nat xs = list_combine (list_mapi (fun i x -> i) xs) xs
+
+let rec list_foldl f acc ys =
+  match ys with [] -> acc | x :: xs -> list_foldl f (f acc x) xs
+
+let compile_naively pats : ir =
   let rec helper_pat scru pat rhs else_top =
     match pat with
     | WildCard -> rhs
     | PConstr (tag, args) ->
-       let dec_args = list_decorate_nat args in
+        let dec_args = list_decorate_nat args in
         let then_ =
-          list_foldl (fun acc z -> match z with (idx, pat1) ->
-              helper_pat (Field (idx, scru)) pat1 acc else_top
-          ) rhs dec_args
+          list_foldl
+            (fun acc z ->
+              match z with
+              | idx, pat1 -> helper_pat (Field (idx, scru)) pat1 acc else_top)
+            rhs dec_args
         in
-       Switch (scru, [(tag,then_)], else_top)
+        Switch (scru, [ (tag, then_) ], else_top)
   in
   let rec helper pats =
     match pats with
     | [] -> Fail
-    | (p,rhs)::ps ->
+    | (p, rhs) :: ps ->
         let else_ = helper ps in
         helper_pat Scru p rhs else_
   in
